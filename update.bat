f337676d6c1c601d62aadbe371fb3aecff00c856
Tom Schumann <schumann.tom@gmail.com>
2016-02-06 14:27:32 +1000

    Updated readme and update procedure now that the repo isn't a GitHub fork.

--- update.bat
+++ update.bat
@@ -1,2 +1,7 @@
+:: make sure the remote is correct
+git remote rm valve
+git remote add valve https://github.com/ValveSoftware/halflife
 :: pull in the latest upstream changes
-git pull https://github.com/ValveSoftware/halflife master
+git pull valve master
+
+pause
