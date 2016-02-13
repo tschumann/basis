25b1092519466497985d70600308cf27cd92f8fa
Tom Schumann <schumann.tom@gmail.com>
2016-02-13 14:07:32 +1000

    Improved script files.

--- build.bat
+++ build.bat
@@ -1,5 +1,20 @@
 :: TODO: build everything
 
+:: don't break any global variables
+SETLOCAL
+
+if "%1" == "release" (
+	SET codeconfiguration=Release
+) else (
+	SET codeconfiguration=Debug
+)
+
+SET visualstudio="C:\Program Files (x86)\Microsoft Visual Studio 9.0\"
+
+:: compile the code
+%visualstudio%\Common7\IDE\devenv.exe cl_dll\cl_dll-2008.sln /Build %codeconfiguration%
+%visualstudio%\Common7\IDE\devenv.exe dlls\hl-2008.sln /Build %codeconfiguration%
+
 :: compile maps
 cd mapsrc
 compile.bat
--- clean.bat
+++ clean.bat
@@ -1,1 +1,17 @@
 
+:: code related stuff
+rmdir /s /q cl_dll\Debug
+rmdir /s /q cl_dll\Release
+rmdir /s /q dlls\Debug
+rmdir /s /q dlls\debughl
+rmdir /s /q dlls\Releasehl
+
+:: map related stuff
+del mapsrc\*.bsp
+del mapsrc\*.rmx
+del mapsrc\*.max
+
+:: wad-related stuff
+del wadsrc\*.wad
+del wadsrc\dev.ls
+del wadsrc\tools.ls
