2505305c06ea194df2795ad9181ac3bb10a57d60
Tom Schumann <schumann.tom@gmail.com>
2016-02-19 20:19:25 +1000

    Added some interfaces.

---
+++ public/ICareerUI.h
@@ -0,0 +1,28 @@
+//========= Copyright © 1996-2002, Valve LLC, All rights reserved. ============
+//
+// Purpose: 
+//
+//=============================================================================
+
+#ifndef ICAREERUI_H
+#define ICAREERUI_H
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include "interface.h"
+#include <vgui\VGUI.h>
+
+using namespace vgui;
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+class ICareerUI : public IBaseInterface
+{
+public:
+};
+
+#define CAREERUI_INTERFACE_VERSION "CareerUI001"
+
+#endif // ICAREERUI_H
