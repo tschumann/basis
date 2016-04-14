6fe62d91529370268d34244c2bafc61c191c46d0
Tom Schumann <schumann.tom@gmail.com>
2016-04-14 21:32:45 +1000

    Updated init scripts with more filesystem testing setup.

--- init.bat
+++ init.bat
@@ -5,9 +5,23 @@ call settings.bat
 mkdir %moddir%\cl_dlls\
 mkdir %moddir%\dlls\
 
+:: make sure qrad has a lights file
 mkdir utils\qrad\Release
 copy mapsrc\lights.rad utils\qrad\Release
 
+:: create directories for filsystem testing assets
 mkdir mod\
+mkdir mod\maps\
+mkdir mod\models\
+mkdir mod\sprites\
+mkdir mod\sounds\
+
+:: create filesystem testing assets
+copy %moddir%\maps\empty.bsp mod\maps\test.bsp
+copy %moddir%\models\null.mdl mod\models\test.mdl
+copy %moddir%\sprites\null.spr mod\sprites\test.spr
+copy %moddir%\sounds\empty.wav mod\sounds\test.wav
+
+:: TODO: check that files with the same names as the filesystem testing assets don't exist in %moddir%
 
 pause
--- init.sh
+++ init.sh
@@ -2,4 +2,17 @@
 mkdir $MODDIR/cl_dlls/
 mkdir $MODDIR/dlls/
 
+# create directories for filsystem testing assets
 mkdir mod/
+mkdir mod/maps/
+mkdir mod/models/
+mkdir mod/sprites/
+mkdir mod/sounds/
+
+# create filesystem testing assets
+cp $MODDIR/maps/empty.bsp mod/maps/test.bsp
+cp $MODDIR/models/null.mdl mod/models/test.mdl
+cp $MODDIR/sprites/null.spr mod/sprites/test.spr
+cp $MODDIR/sounds/empty.wav mod/sounds/test.wav
+
+# TODO: check that files with the same names as the filesystem testing assets don't exist in $MODDIR
