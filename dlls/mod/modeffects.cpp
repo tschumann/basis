65fb78ff0a4526034748c97761cc9109b86926dd
Tom Schumann <schumann.tom@gmail.com>
2016-08-21 10:01:46 +1000

    Moved env_fog into modeffects.cpp

---
+++ dlls/mod/modeffects.cpp
@@ -0,0 +1,62 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "monsters.h"
+#include "customentity.h"
+#include "effects.h"
+#include "weapons.h"
+#include "decals.h"
+#include "func_break.h"
+#include "shake.h"
+
+//=========================================================
+// Fog
+//=========================================================
+#define SF_IN_SKYBOX	0x0001
+
+LINK_ENTITY_TO_CLASS( env_fog, CClientFog );
+
+void CClientFog::KeyValue( KeyValueData *pkvd )
+{
+	if( FStrEq( pkvd->szKeyName, "startdist" ) )
+	{
+		m_iStartDist = atoi( pkvd->szValue );
+		pkvd->fHandled = TRUE;
+	}
+	else if( FStrEq( pkvd->szKeyName, "enddist" ) )
+	{
+		m_iEndDist = atoi( pkvd->szValue );
+		pkvd->fHandled = TRUE;
+	}
+	else if( FStrEq( pkvd->szKeyName, "density" ) )
+	{
+		m_iDensity = atof( pkvd->szValue );
+		pkvd->fHandled = TRUE;
+	}
+	else
+	{
+		CBaseEntity::KeyValue( pkvd );
+	}
+}
+
+void CClientFog::Spawn( void )
+{
+	pev->movetype = MOVETYPE_NOCLIP;
+	pev->solid = SOLID_NOT;							// Remove model & collisions
+	pev->renderamt = 0;								// The engine won't draw this model if this is set to 0 and blending is on
+	pev->rendermode = kRenderTransTexture;
+}
