5da06bcd2fdee476b58b9464942d33b578eb9f0f
tschumann <schumann.tom@gmail.com>
2018-07-28 12:04:13 +1000

    Split model-based chargers into separate classes and updated the license.

--- dlls/mod/modchargers.cpp
+++ dlls/mod/modchargers.cpp
@@ -21,29 +21,56 @@
 #include "gamerules.h"
 
 //-------------------------------------------------------------
-// Wall mounted charger
+// Wall mounted health kit
 //-------------------------------------------------------------
-class CBaseCharger : public CBaseToggle
+class CHealthCharger : public CBaseToggle
 {
 public:
-	virtual void Spawn( );
+	void Spawn( );
+	void Precache( void );
 	void EXPORT Off(void);
-	virtual void EXPORT Recharge(void) {}
-	void KeyValue( KeyValueData *pkvd ) {}
-	void Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value ) {}
+	void EXPORT Recharge(void);
+	void KeyValue( KeyValueData *pkvd );
+	void Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
 	virtual int	ObjectCaps( void ) { return (CBaseToggle :: ObjectCaps() | FCAP_CONTINUOUS_USE) & ~FCAP_ACROSS_TRANSITION; }
+	virtual int		Save( CSave &save );
+	virtual int		Restore( CRestore &restore );
+
+	static	TYPEDESCRIPTION m_SaveData[];
 
 	float m_flNextCharge; 
 	int		m_iReactivate ; // DeathMatch Delay until reactvated
 	int		m_iJuice;
 	int		m_iOn;			// 0 = off, 1 = startup, 2 = going
 	float   m_flSoundTime;
-
-	string_t m_iszStopSound;
-	float m_flRechargeTime;
 };
 
-void CBaseCharger::Spawn()
+TYPEDESCRIPTION CHealthCharger::m_SaveData[] =
+{
+	DEFINE_FIELD( CHealthCharger, m_flNextCharge, FIELD_TIME),
+	DEFINE_FIELD( CHealthCharger, m_iReactivate, FIELD_INTEGER),
+	DEFINE_FIELD( CHealthCharger, m_iJuice, FIELD_INTEGER),
+	DEFINE_FIELD( CHealthCharger, m_iOn, FIELD_INTEGER),
+	DEFINE_FIELD( CHealthCharger, m_flSoundTime, FIELD_TIME),
+};
+
+IMPLEMENT_SAVERESTORE( CHealthCharger, CBaseEntity );
+
+LINK_ENTITY_TO_CLASS(item_healthcharger, CHealthCharger);
+
+
+void CHealthCharger::KeyValue( KeyValueData *pkvd )
+{
+	if (FStrEq(pkvd->szKeyName, "dmdelay"))
+	{
+		m_iReactivate = atoi(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else
+		CBaseToggle::KeyValue( pkvd );
+}
+
+void CHealthCharger::Spawn()
 {
 	Precache( );
 
@@ -52,60 +79,9 @@ void CBaseCharger::Spawn()
 
 	UTIL_SetOrigin(pev, pev->origin);		// set size and link into world
 	UTIL_SetSize(pev, pev->mins, pev->maxs);
-	SET_MODEL(ENT(pev), STRING(pev->model) );	
-}
+	SET_MODEL(ENT(pev), STRING(pev->model) );
+	m_iJuice = gSkillData.healthchargerCapacity;		
 
-void CBaseCharger::Off(void)
-{
-	// Stop looping sound.
-	if (m_iOn > 1)
-		STOP_SOUND( ENT(pev), CHAN_STATIC, STRING(m_iszStopSound) );
-
-	m_iOn = 0;
-
-	if ((!m_iJuice) &&  ( ( m_iReactivate = m_flRechargeTime ) > 0) )
-	{
-		pev->nextthink = pev->ltime + m_iReactivate;
-		SetThink(&CBaseCharger::Recharge);
-	}
-	else
-		SetThink( &CBaseCharger::SUB_DoNothing );
-}
-
-
-class CHealthCharger : public CBaseCharger
-{
-public:
-	void Spawn( );
-	void Precache( void );
-	void EXPORT Recharge(void);
-
-	virtual int		Save( CSave &save );
-	virtual int		Restore( CRestore &restore );
-
-	static	TYPEDESCRIPTION m_SaveData[];
-};
-
-TYPEDESCRIPTION CHealthCharger::m_SaveData[] =
-{
-	DEFINE_FIELD( CHealthCharger, m_flNextCharge, FIELD_TIME ),
-	DEFINE_FIELD( CHealthCharger, m_iReactivate, FIELD_INTEGER),
-	DEFINE_FIELD( CHealthCharger, m_iJuice, FIELD_INTEGER),
-	DEFINE_FIELD( CHealthCharger, m_iOn, FIELD_INTEGER),
-	DEFINE_FIELD( CHealthCharger, m_flSoundTime, FIELD_TIME ),
-};
-
-IMPLEMENT_SAVERESTORE( CHealthCharger, CBaseEntity );
-
-LINK_ENTITY_TO_CLASS(item_healthcharger, CHealthCharger);
-
-
-void CHealthCharger::Spawn()
-{
-	CBaseCharger::Spawn(  );
-	m_iJuice = gSkillData.healthchargerCapacity;
-	m_iszStopSound = MAKE_STRING("items/medcharge4.wav");
-	m_flRechargeTime = g_pGameRules->FlHealthChargerRechargeTime();
 }
 
 void CHealthCharger::Precache()
@@ -115,6 +91,65 @@ void CHealthCharger::Precache()
 	PRECACHE_SOUND("items/medcharge4.wav");
 }
 
+
+void CHealthCharger::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
+{ 
+	// Make sure that we have a caller
+	if (!pActivator)
+		return;
+	// if it's not a player, ignore
+	if ( !pActivator->IsPlayer() )
+		return;
+
+	// if there is no juice left, turn it off
+	if (m_iJuice <= 0)
+	{		
+		Off();
+	}
+
+	// if the player doesn't have the suit, or there is no juice left, make the deny noise
+	if ((m_iJuice <= 0) || (!(pActivator->pev->weapons & (1<<WEAPON_SUIT))))
+	{
+		if (m_flSoundTime <= gpGlobals->time)
+		{
+			m_flSoundTime = gpGlobals->time + 0.62;
+			EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/medshotno1.wav", 1.0, ATTN_NORM );
+		}
+		return;
+	}
+
+	pev->nextthink = pev->ltime + 0.25;
+	SetThink(&CHealthCharger::Off);
+
+	// Time to recharge yet?
+
+	if (m_flNextCharge >= gpGlobals->time)
+		return;
+
+	// Play the on sound or the looping charging sound
+	if (!m_iOn)
+	{
+		m_iOn++;
+		EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/medshot4.wav", 1.0, ATTN_NORM );
+		m_flSoundTime = 0.56 + gpGlobals->time;
+	}
+	if ((m_iOn == 1) && (m_flSoundTime <= gpGlobals->time))
+	{
+		m_iOn++;
+		EMIT_SOUND(ENT(pev), CHAN_STATIC, "items/medcharge4.wav", 1.0, ATTN_NORM );
+	}
+
+
+	// charge the player
+	if ( pActivator->TakeHealth( 1, DMG_GENERIC ) )
+	{
+		m_iJuice--;
+	}
+
+	// govern the rate of charge
+	m_flNextCharge = gpGlobals->time + 0.1;
+}
+
 void CHealthCharger::Recharge(void)
 {
 		EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/medshot4.wav", 1.0, ATTN_NORM );
@@ -122,18 +157,45 @@ void CHealthCharger::Recharge(void)
 	SetThink( &CHealthCharger::SUB_DoNothing );
 }
 
+void CHealthCharger::Off(void)
+{
+	// Stop looping sound.
+	if (m_iOn > 1)
+		STOP_SOUND( ENT(pev), CHAN_STATIC, "items/medcharge4.wav" );
 
-class CArmorCharger : public CBaseCharger
+	m_iOn = 0;
+
+	if ((!m_iJuice) &&  ( ( m_iReactivate = g_pGameRules->FlHealthChargerRechargeTime() ) > 0) )
+	{
+		pev->nextthink = pev->ltime + m_iReactivate;
+		SetThink(&CHealthCharger::Recharge);
+	}
+	else
+		SetThink( &CHealthCharger::SUB_DoNothing );
+}
+
+
+
+class CArmorCharger : public CBaseToggle
 {
 public:
 	void Spawn( );
 	void Precache( void );
+	void EXPORT Off(void);
 	void EXPORT Recharge(void);
-
+	void KeyValue( KeyValueData *pkvd );
+	void Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
+	virtual int	ObjectCaps( void ) { return (CBaseToggle :: ObjectCaps() | FCAP_CONTINUOUS_USE) & ~FCAP_ACROSS_TRANSITION; }
 	virtual int		Save( CSave &save );
 	virtual int		Restore( CRestore &restore );
 
 	static	TYPEDESCRIPTION m_SaveData[];
+
+	float m_flNextCharge; 
+	int		m_iReactivate ; // DeathMatch Delay until reactvated
+	int		m_iJuice;
+	int		m_iOn;			// 0 = off, 1 = startup, 2 = going
+	float   m_flSoundTime;
 };
 
 TYPEDESCRIPTION CArmorCharger::m_SaveData[] =
@@ -150,12 +212,28 @@ IMPLEMENT_SAVERESTORE( CArmorCharger, CBaseEntity );
 LINK_ENTITY_TO_CLASS(item_recharge, CArmorCharger);
 
 
+void CArmorCharger::KeyValue( KeyValueData *pkvd )
+{
+	if (FStrEq(pkvd->szKeyName, "dmdelay"))
+	{
+		m_iReactivate = atoi(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else
+		CBaseToggle::KeyValue( pkvd );
+}
+
 void CArmorCharger::Spawn()
 {
-	CBaseCharger::Spawn(  );
+	Precache( );
+
+	pev->solid		= SOLID_BSP;
+	pev->movetype	= MOVETYPE_PUSH;
+
+	UTIL_SetOrigin(pev, pev->origin);		// set size and link into world
+	UTIL_SetSize(pev, pev->mins, pev->maxs);
+	SET_MODEL(ENT(pev), STRING(pev->model) );
 	m_iJuice = gSkillData.suitchargerCapacity;		
-	m_iszStopSound = MAKE_STRING("items/suitcharge1.wav");
-	m_flRechargeTime = g_pGameRules->FlHEVChargerRechargeTime();
 }
 
 void CArmorCharger::Precache()
@@ -165,8 +243,96 @@ void CArmorCharger::Precache()
 	PRECACHE_SOUND("items/suitchargeok1.wav");
 }
 
+
+void CArmorCharger::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
+{ 
+	// if it's not a player, ignore
+	if (!FClassnameIs(pActivator->pev, "player"))
+		return;
+
+	// if there is no juice left, turn it off
+	if (m_iJuice <= 0)
+	{	
+		Off();
+	}
+
+	// if the player doesn't have the suit, or there is no juice left, make the deny noise
+	if ((m_iJuice <= 0) || (!(pActivator->pev->weapons & (1<<WEAPON_SUIT))))
+	{
+		if (m_flSoundTime <= gpGlobals->time)
+		{
+			m_flSoundTime = gpGlobals->time + 0.62;
+			EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/suitchargeno1.wav", 0.85, ATTN_NORM );
+		}
+		return;
+	}
+
+	pev->nextthink = pev->ltime + 0.25;
+	SetThink(&CArmorCharger::Off);
+
+	// Time to recharge yet?
+
+	if (m_flNextCharge >= gpGlobals->time)
+		return;
+
+	// Make sure that we have a caller
+	if (!pActivator)
+		return;
+
+	m_hActivator = pActivator;
+
+	//only recharge the player
+
+	if (!m_hActivator->IsPlayer() )
+		return;
+	
+	// Play the on sound or the looping charging sound
+	if (!m_iOn)
+	{
+		m_iOn++;
+		EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/suitchargeok1.wav", 0.85, ATTN_NORM );
+		m_flSoundTime = 0.56 + gpGlobals->time;
+	}
+	if ((m_iOn == 1) && (m_flSoundTime <= gpGlobals->time))
+	{
+		m_iOn++;
+		EMIT_SOUND(ENT(pev), CHAN_STATIC, "items/suitcharge1.wav", 0.85, ATTN_NORM );
+	}
+
+
+	// charge the player
+	if (m_hActivator->pev->armorvalue < 100)
+	{
+		m_iJuice--;
+		m_hActivator->pev->armorvalue += 1;
+
+		if (m_hActivator->pev->armorvalue > 100)
+			m_hActivator->pev->armorvalue = 100;
+	}
+
+	// govern the rate of charge
+	m_flNextCharge = gpGlobals->time + 0.1;
+}
+
 void CArmorCharger::Recharge(void)
 {
 	m_iJuice = gSkillData.suitchargerCapacity;
 	SetThink( &CArmorCharger::SUB_DoNothing );
+}
+
+void CArmorCharger::Off(void)
+{
+	// Stop looping sound.
+	if (m_iOn > 1)
+		STOP_SOUND( ENT(pev), CHAN_STATIC, "items/suitcharge1.wav" );
+
+	m_iOn = 0;
+
+	if ((!m_iJuice) &&  ( ( m_iReactivate = g_pGameRules->FlHEVChargerRechargeTime() ) > 0) )
+	{
+		pev->nextthink = pev->ltime + m_iReactivate;
+		SetThink(&CArmorCharger::Recharge);
+	}
+	else
+		SetThink( &CArmorCharger::SUB_DoNothing );
 }
