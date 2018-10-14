a84c1d6c5ce3a998f793ad3c3cfd3720eac7bf2d
tschumann <schumann.tom@gmail.com>
2018-10-14 10:34:32 +1000

    Fixed missing asset swapping blocking brush models from loading.

--- dlls/mod/modcbase.cpp
+++ dlls/mod/modcbase.cpp
@@ -26,9 +26,9 @@ int ModDispatchSpawn( edict_t *pent )
 	{
 		const char *szModelName = STRING(pEntity->pev->model);
 
-		// swap the model name as spawn time - overriding the model name that gets passed to engine
-		// functions works but the wrong model name in entvars_t still causes problems in the engine
-		if ( strlen( szModelName ) && g_pFileSystem && !g_pFileSystem->FileExists( szModelName ) )
+		// swap the model name at spawn time (but not for brush models) - overriding the model name that gets passed
+		// to engine functions works but the wrong model name in entvars_t ends up causing problems in the engine
+		if ( strlen( szModelName ) && szModelName[0] != '*' && g_pFileSystem && !g_pFileSystem->FileExists( szModelName ) )
 		{
 			ALERT( at_warning, "%s doesn't exist - changing %s to use models/null.mdl instead\n", szModelName, STRING(pEntity->pev->classname) );
 			pEntity->pev->model = MAKE_STRING( "models/null.mdl" );
