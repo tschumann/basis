c829b2a9f3a17a189cc88caace24125e488787de
tschumann <schumann.tom@gmail.com>
2022-05-28 10:57:41 +1000

    Fixed off by one error in SENTENCEG_Init.

--- dlls/sound.cpp
+++ dlls/sound.cpp
@@ -1311,7 +1311,7 @@ void SENTENCEG_Init()
 		if (!buffer[j])
 			continue;
 
-		if (gcallsentences > CVOXFILESENTENCEMAX)
+		if (gcallsentences >= CVOXFILESENTENCEMAX)
 		{
 			ALERT (at_error, "Too many sentences in sentences.txt!\n");
 			break;
